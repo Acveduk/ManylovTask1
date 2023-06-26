adcbca9c4a82ef772887e12f25b5d488fb558a09
Vlad <VLAD26.rus21@gmail.com>
2023-06-26 15:43:11 +0300

    2

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from moviepy.editor import *
 from moviepy.config import change_settings
 from setting import *
 
-
 change_settings({"IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"})
 
 
@@ -11,13 +10,13 @@ def ticker(text, color, font, img):
     img = ImageClip(img).set_duration(3)  # Создаем видео из изображения
     txt = TextClip(txt=text, color=color, font=font, fontsize=70).set_duration(
         3)  # Создаем видео с текстом и настраиваем его
+    size = txt.size[0] if txt.size[0] >= 100 else 100
 
-    video = CompositeVideoClip([img, txt.set_pos(lambda t: (100 - t * (len(text) * 17.5), 'center'))],
-                             size=screensize)  # Объединяем 2 видео в одно
+    video = CompositeVideoClip([img, txt.set_pos(lambda t: (0 - t * ((size-99)/3), 'center'))],
+                               size=screensize)  # Объединяем 2 видео в одно
 
+    # lambda t: (100 - t, 'center')
     video.write_videofile(f'video.mp4', fps=60, codec='mpeg4')  # записываем видео в файл
-
-
 
 
 print("БЕГУЩАЯ СТРОКА\n\n"
@@ -27,4 +26,3 @@ color = color()
 font = font()
 
 ticker(text, color, font, "background.jpg")
-
